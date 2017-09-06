002198083860bf7e37eca908d47d250f2dde585f
Eric Austin <ewaustin@gmail.com>
2017-09-05 23:57:33 -0500

    base mode done

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 console.log('script sourced');
+
+const maxVoltage = 1000;
+
+class ElectricDrill {
+  constructor(brand, color, currentCharge, torque) {
+    this.brand = brand;
+    this.color = color;
+    this.currentCharge = currentCharge;
+    this.torque = torque;
+  }
+
+
+  charge(volts) {
+    if (volts + this.currentCharge <= maxVoltage) {
+      this.currentCharge += volts;
+    } else {
+      console.log('cannot overcharge', this.color, this.brand);
+
+    }
+  }
+}
+
+let sabrinasDrill = new ElectricDrill("Ryobi", "Green", 650, 55);
+let ericsDrill = new ElectricDrill("Milwaukee", "Red", 430, 75);
+
+console.log(sabrinasDrill);
+console.log(ericsDrill);
+
+
+
+console.log("Sabrina's maxVoltage", maxVoltage);
+console.log("Sabrina's currentCharge", sabrinasDrill.currentCharge);
+console.log("Sabrina's additional charge", 200);
+sabrinasDrill.charge(200);
+console.log("Sabrina's current charge", sabrinasDrill.currentCharge);
+
+console.log("Eric's maxVoltage", maxVoltage);
+console.log("Eric's currentCharge", ericsDrill.currentCharge);
+console.log("Eric's additional charge", 600);
+ericsDrill.charge(600);
+console.log("Eric's current charge", ericsDrill.currentCharge);
